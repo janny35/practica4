4c834e68688356516cdee4c3637f5c0da9da71b9
Janneth Carballo Chambi <80425872+janny35@users.noreply.github.com>
2022-09-01 23:25:13 -0400

    modificando el archivo demo.js

--- demo.js
+++ demo.js
@@ -1,6 +1,6 @@
 console.log ("hola mundo");
 console.log ("Hola Clase");
 console.log ("Diplomado FullStack");
-console.log ("Hola clase dia 6  - 25/08/2022");
-console.log ("Diplomado FullStack");
+console.log ("Buena noches clase dia 6  - 25/08/2022");
+console.log ("Diplomado Fullstack");
 
